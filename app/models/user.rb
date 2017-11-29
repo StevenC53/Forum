52273dc34ab83078dce20ef35c4c45e805762b40
Steven Connors <steven.connors53@gmail.com>
2017-11-29 12:31:00 -0500

    finishing touches.  added username capabilities to users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
+#
+# https://github.com/plataformatec/devise/wiki/How-To:-Allow-users-to-sign-in-using-their-username-or-email-address
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
@@ -5,4 +8,20 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :comments, dependent: :destroy
   has_many :posts, dependent: :destroy
+  attr_accessor :login
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+        where(conditions.to_h).first
+      end
+    end
+    validate :validate_username
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
 end
