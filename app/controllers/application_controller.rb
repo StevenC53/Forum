aa522c444cce2b03dfac2c4d2246636d6a732192
Steven Connors <steven.connors53@gmail.com>
2017-11-30 11:13:45 -0500

    fixes breaking app on duplicate username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  rescue_from ActiveRecord::RecordNotUnique, with: :record_not_unique
 
   before_action :authenticate_user!, except: [:index, :show]
   before_action :configure_permitted_parameters, if: :devise_controller?
   protected
-
+  def record_not_unique
+    redirect_to new_user_registration_path, notice: "That username is already taken."
+  end
   def configure_permitted_parameters
     added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
